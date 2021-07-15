2626346777739c64ae168760119a12cd681be8f3
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-15 21:24:15 +0200

    Add composer.json and SettingsServiceProvider.php

---
+++ src/SettingsServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Latus\Settings;
+
+use Illuminate\Support\ServiceProvider;
+use Latus\Plugins\Repositories\Eloquent\SettingRepository;
+use Latus\Settings\Repositories\Contracts\SettingRepository as SettingRepositoryContract;
+
+class SettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (!$this->app->bound(SettingRepositoryContract::class)) {
+            $this->app->bind(SettingRepositoryContract::class, SettingRepository::class);
+        }
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+}
