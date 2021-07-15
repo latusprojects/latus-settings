3f8d55eb4d175c72434f4492c423bb07a74645e0
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-07-15 21:25:22 +0200

    Add Setting.php model and migration

---
+++ src/Models/Setting.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Latus\Settings\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+
+    protected $fillable = [
+        'key', 'value', 'value_long',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
+    ];
+
+    public function getValue()
+    {
+        return $this->value ?? $this->value_long;
+    }
+
+    public function setValue(string $value)
+    {
+        if (strlen($value) <= 255) {
+            $this->value_long = '';
+            $this->value = $value;
+            return;
+        }
+        $this->value = null;
+        $this->value_long = $value;
+    }
+
+}
