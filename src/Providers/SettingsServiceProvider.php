08c3a690c2d27d62790cde93a6862ad7fabf21dd
steelvirus <35262769+steelvirus@users.noreply.github.com>
2021-08-02 13:46:01 +0200

    Fix invalid relative path

--- src/Providers/SettingsServiceProvider.php
+++ src/Providers/SettingsServiceProvider.php
@@ -27,6 +27,6 @@ class SettingsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 }
